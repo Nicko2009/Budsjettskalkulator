74edc5388193025e879e8111f62c08720aeb8560
Nicko2009 <niwea005@osloskolen.no>
2026-09-21 13:31:34 +0200

    javascript
    henter elementene fra html til javascript

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const intekter = document.getElementById("inntekter");
+const intekterBelop = document.getElementById("IntekterBelop");
+const leggTilInntekt = document.getElementById("leggTilInntekt");
+
+
+const utgifter = document.getElementById("utgifter");
+const utgiftBelop = document.getElementById("UtgifterBelop");
+const leggTilUtgift = document.getElementById("leggTilUtgift");
+
+
+const saldo = document.getElementById("saldo");
+
+
+
+
+
+
+
