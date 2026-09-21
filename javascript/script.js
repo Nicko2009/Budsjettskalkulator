b1a38fdcd2507313a6e183f28e4cac2e5f06e4df
Nicko2009 <niwea005@osloskolen.no>
2026-09-21 14:16:27 +0200

    inntekter
    regristere intekter og beløpet og legger det til i listen med andre intekter

--- javascript/script.js
+++ javascript/script.js
@@ -2,17 +2,29 @@ const intekter = document.getElementById("inntekter");
 const intekterBelop = document.getElementById("IntekterBelop");
 const leggTilInntekt = document.getElementById("leggTilInntekt");
 
-
-const utgifter = document.getElementById("utgifter");
+const utgift = document.getElementById("utgifter");
 const utgiftBelop = document.getElementById("UtgifterBelop");
 const leggTilUtgift = document.getElementById("leggTilUtgift");
+
+const innekter = document.getElementById("inntekter");
+const utgifter = document.getElementById("inntekter");
 
 
 const saldo = document.getElementById("saldo");
 
+let totalInntekter = 0;
+let totalUtgifter = 0;
+let totalSaldo = 0;
 
+leggTilInntekt.addEventListener("click", function() {
+    
+    const navn = intekter.value;
+     const belop = intekterBelop.value;
+     const nyInntekt = document.createElement("li");
 
-
+     nyInntekt.textContent = navn + " " + belop + "kr"
+     intekter.appendChild(nyInntekt);
+});
 
 
 
