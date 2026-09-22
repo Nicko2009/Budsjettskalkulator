eea43cc8edde691a39848b4d003e4c204fd0d460
Nicko2009 <niwea005@osloskolen.no>
2026-09-22 08:40:48 +0200

    sum intekter og utgifter
    regner ut utgifter og inntekter

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+
 const inntekt = document.getElementById("inntekt");
 const inntektBelop = document.getElementById("IntekterBelop");
 const leggTilInntekt = document.getElementById("leggTilInntekt");
@@ -10,28 +11,36 @@ const inntekter = document.getElementById("inntekter");
 const utgifter = document.getElementById("utgifter");
 
 const saldo = document.getElementById("saldo");
+const sumInntekter = document.getElementById("sumInntekter")
+const sumUtgifter = document.getElementById("sumUtgifter")
 
 let totalInntekter = 0;
 let totalUtgifter = 0;
-let totalSaldo = 0;
+let totalSaldo = 0;                             
 
 leggTilInntekt.addEventListener("click", function() {
     const navn = inntekt.value;
-    const belop = inntektBelop.value;
+    const belop = Number(inntektBelop.value);
     const nyInntekt = document.createElement("li");
 
     nyInntekt.textContent = navn + " " + belop + " kr";
     inntekter.appendChild(nyInntekt);
+    totalInntekter += belop 
+    sumInntekter.textContent = "sum inntekter" + totalInntekter + "kr"
 });
-
-
 
 leggTilUtgift.addEventListener("click", function() {
     
     const navn = utgift.value;
-     const belop = utgiftBelop.value;
+     const belop = Number(utgiftBelop.value);
      const nyUtgifter = document.createElement("li");
 
      nyUtgifter.textContent = navn + " " + belop + "kr"
      utgifter.appendChild(nyUtgifter);
+    totalUtgifter += belop;
+        sumUtgifter.textContent = "sum utgifter" + totalUtgifter + "kr"
+
 });
+
+
+
