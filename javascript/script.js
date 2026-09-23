f0b9a3818cfaf417a5ce4a423a8cb8d9a3963dec
Nicko2009 <niwea005@osloskolen.no>
2026-09-23 14:13:13 +0200

    object liste
    brukte objest liste for at siden kan hente in lister som har blit lagret fra før av

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,10 @@
+let inntekterData = JSON.parse(localStorage.getItem("inntekter")) || [];
+let utgifterData = JSON.parse(localStorage.getItem("utgifter")) || [];
+
+let totalInntekter = Number(localStorage.getItem("totalInntekter")) || 0;
+let totalUtgifter = Number(localStorage.getItem("totalUtgifter")) || 0;
+
+
 const inntekt = document.getElementById("inntekt");
 const inntektBelop = document.getElementById("IntekterBelop");
 const leggTilInntekt = document.getElementById("leggTilInntekt");
@@ -13,12 +20,17 @@ const saldo = document.getElementById("saldo");
 const sumInntekter = document.getElementById("sumInntekter")
 const sumUtgifter = document.getElementById("sumUtgifter")
 
-let totalInntekter = JSON.parse(localStorage.getItem("totalInntekter")) || 0;
-let totalUtgifter = JSON.parse(localStorage.getItem("totalUtgifter")) || 0;
-
 leggTilInntekt.addEventListener("click", function() {
     const navn = inntekt.value;
     const belop = Number(inntektBelop.value);
+
+    inntekterData.push({
+        navn: navn,
+        belop: belop
+    });
+    localStorage.setItem("inntekter", JSON.stringify(inntekterData));
+
+
     const nyInntekt = document.createElement("li");
     nyInntekt.textContent = navn + " " + belop + " kr";
     inntekter.appendChild(nyInntekt);
@@ -33,6 +45,14 @@ leggTilInntekt.addEventListener("click", function() {
 leggTilUtgift.addEventListener("click", function() {
     const navn = utgift.value;
     const belop = Number(utgiftBelop.value);
+
+    utgifterData.push({
+        navn: navn,
+        belop: belop
+    });
+    localStorage.setItem("utgifter", JSON.stringify(utgifterData));
+
+
     const nyUtgifter = document.createElement("li");
     nyUtgifter.textContent = navn + "   - " + belop + "kr"
     utgifter.appendChild(nyUtgifter);
@@ -67,8 +87,30 @@ function fjernUtgifter() {
 
     localStorage.setItem("totalUtgifter", JSON.stringify(totalUtgifter));
 
-
     sumUtgifter.textContent = "sum utgifter 0 kr"
     oppdaterSaldo();
 }
 
+inntekterData.forEach(function(inntekt) {
+    const nyInntekter = document.createElement("li");
+
+    nyInntekter.textContent =
+    inntekt.navn + " " + inntekt.belop + " kr";
+
+    inntekter.appendChild(nyInntekter);
+});
+
+utgifterData.forEach(function(utgift) {
+    const nyUtgifter = document.createElement("li");
+
+    nyUtgifter.textContent =
+    utgift.navn + " " + utgift.belop + " kr"
+
+    utgifter.appendChild(nyUtgifter)
+
+}); 
+sumInntekter.textContent = "sum inntekter" + totalInntekter + " kr";
+
+sumUtgifter.textContent = "sum utgifter" + totalUtgifter + " kr";
+
+oppdaterSaldo()
